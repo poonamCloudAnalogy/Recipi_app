78661fe767ee20f8411793ddb971cdcac4d07d63
Poonam <poonamnishad@forcebolt.com>
2021-02-11 13:04:15 +0530

    added fieldsets in admin.py

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -26,10 +26,15 @@ class AdminSiteTests(TestCase):
         self.assertContains(res,self.user.name)
         self.assertContains(res,self.user.email)
 
-def test_user_change_page(self):
-    """test that the user edit page"""
-    url = reverse('admin:core_user_change',args=[self.user.id])
-    res=self.client.get(url)
+    def test_user_change_page(self):
+         """test that the user edit page"""
+         url = reverse('admin:core_user_change',args=[self.user.id])
+         res=self.client.get(url)
 
-    self.asserEqual(res.status_code,200)
-    
+         self.assertEqual(res.status_code,200)
+
+    def test_create_user_page(self):
+        url=reverse('admin:core_user_add')
+        res=self.client.get(url)
+
+        self.assertEqual(res.status_code,200)
